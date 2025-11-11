4c7c67268487875b5d6fe8b994dfe52a56b306c8
Joy Wanjiru Gaitho <cirujoygaitho7103@gmail.com>
2025-11-11 20:17:48 +0300

    drawing patterns with nested loops

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,19 @@
+# square parttern generator
+def pattern():
+    # prompt user to enter a positive number
+    try:
+        size = int(input("Enter the size of the pattern:"))
+        if size <= 0:
+            print("Please enter a positive interger.")
+            return
+    except ValueError:
+        print("Invalid input. Please enter a whole number.")
+        return
+    # initialize the row counter
+    row = 0
+    while row < size: # this loop ensures the processes of printing a new row and moving to the next line is repeated exactly "size" number of times. eg 
+        for col in range(size):
+            print("*", end="")
+        print()
+        row += 1
+pattern()
