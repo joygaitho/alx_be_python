cab654f936db870535f46619bffee43c8284bd35
Joy Wanjiru Gaitho <cirujoygaitho7103@gmail.com>
2025-11-11 19:02:20 +0300

    simple calculator with match case

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,29 @@
+# simple calculator using match case
+def calculator():
+    # prompt user input
+    try:
+        num1 = int(input("Enter the first number:"))
+        num2 = int(input("Enter the second number:"))
+    except ValueError:
+        print("Invalid input! Please enter an integer.")
+        return 
+    operation = input("Choose the operation (+, -, *, /):")
+    match operation:
+        case '+':
+            result = num1 + num2
+            print(f"The result is {result}.")
+        case '-':
+            result = num1 - num2
+            print(f"The result is {result}.")
+        case '*':
+            result = num1 * num2
+            print(f"The result is {result:,}.")
+        case '/':
+            if num2 == 0:
+                print("Cannot divide by zero.")
+            else:
+                result = num1 / num2
+                print(f"The result is {result:,.0f}.")
+        case _:
+            print("Invalid operation. Please choose one of +, -, * or /.")
+calculator()
